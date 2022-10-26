da9f531c91adef3543cc4977a62a7ae1c72f4bbf
pisa <pisa0401mst@gmail.com>
2022-10-26 23:53:28 +0900

    useState追加

--- renderer/pages/about.tsx
+++ renderer/pages/about.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import Image from 'next/image'
+import { useState } from "react";
 
-const AboutPage = () => (
+const AboutPage = () =>  {
+  const [isShow, setIsShow] = useState(false);
+  return (
   <Layout title="About | Next.js + TypeScript + Electron Example">
     <h1>About</h1>
     <p>This is the about page</p>
@@ -10,10 +13,16 @@ const AboutPage = () => (
       <Link href="/">
         <a>Go home</a>
       </Link>
+      <div>
+        <h1>{isShow ? `ON` : `OFF`}</h1>
+      </div>
+      <div>
+        <button onClick={()=> setIsShow(true)}>change</button>
+      </div>
     </p>
     <Image src="/images/loading-l-5.gif" width={64} height={64}/>
     <Image src="/images/run_gopher.ico" width={128} height={128}/>
   </Layout>
-)
+)};
 
 export default AboutPage
